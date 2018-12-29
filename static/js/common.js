b1732b7a3648851b3f994d1f09fb108d9f919958
Lee <lihiqi@kangehao.com>
2018-12-29 11:34:23 +0800

    联系客服打开新窗口遗漏

--- static/js/common.js
+++ static/js/common.js
@@ -4,7 +4,6 @@ document.documentElement.style.fontSize = deviceWidth / 7.5 + 'px';
 
 export default {
   linkServer() {
-    console.log('aaa')
     window.open("https://web.jiaxincloud.com/gray/mobile.html?&thirdJson={}&bg=FD8159&dialogLogo=0&dialogType=1&dialogMode=1&lang=cn&blinkTitle=1&orgName=mglhodd3enu2mg&appName=kgh431&appChannel=20003&quoteUrl=https://web.jiaxincloud.com&pageTitle=看个号客服&pageUrl=https://web.jiaxincloud.com/onekey.html?id=mglhodd3enu2mg&appName=kgh431&appChannel=20003&alone=1&jump=true&jump=true");
   }
 }
